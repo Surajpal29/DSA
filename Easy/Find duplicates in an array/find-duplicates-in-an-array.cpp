b708463eed302b26806d178301943b758061c994
suraj pal <102373413+Surajpal29@users.noreply.github.com>
2023-11-08 12:06:33 +0530

    Updated Solution - GfG to GitHub

--- Easy/Find duplicates in an array/find-duplicates-in-an-array.cpp
+++ Easy/Find duplicates in an array/find-duplicates-in-an-array.cpp
@@ -5,19 +5,19 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
   public:
-    vector<int> duplicates(int arr[], int n) {
+    vector<int> duplicates(long long arr[], int n) {
         // code here
-      vector<int>ans;
-      for(int i=0;i<n;i++){
-          int index=arr[i]%n;
-          arr[index]+=n;
-      }
-      for(int i=0;i<n;i++){
-          if((arr[i]/n)>=2)
-            ans.push_back(i);
-      }
-      if(ans.size()==0) return {-1};
-      return ans;
+        vector<int>ans,cnt(n,0);
+        
+        for(int i=0;i<n;i++)  cnt[arr[i]]++;
+        
+        for(int i=0;i<n;i++){
+            if(cnt[i]>1){
+                ans.push_back(i);
+            }
+        }
+        if(ans.size()) return ans;
+        return {-1};
     }
 };
 
@@ -29,7 +29,7 @@ int main() {
     while (t-- > 0) {
         int n;
         cin >> n;
-        int a[n];
+        long long a[n];
         for (int i = 0; i < n; i++) cin >> a[i];
         Solution obj;
         vector<int> ans = obj.duplicates(a, n);
