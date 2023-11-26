5bb01c5489f1e191dfff0909684930dc931e5524
suraj pal <102373413+Surajpal29@users.noreply.github.com>
2023-11-26 20:16:03 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Print Pattern/print-pattern.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+int solve(vector<int>&ans,int n){
+       if(n>0){ ans.push_back(n);
+        ans.push_back(solve(ans,n-5));
+           return n+5;
+       }
+        else
+{        ans.push_back(n);
+    return n+5;
+}
+        
+    }
+    vector<int> pattern(int N){
+        // code here
+          vector<int>ans;
+        int s=solve(ans,N);
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        vector<int> ans = ob.pattern(N);
+        for(int u: ans)
+            cout<<u<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
