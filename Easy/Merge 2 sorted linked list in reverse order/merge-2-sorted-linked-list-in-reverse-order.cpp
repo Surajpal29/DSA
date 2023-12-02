7e52834d144451a96f306b229e335f0f278f9fba
suraj pal <102373413+Surajpal29@users.noreply.github.com>
2023-12-02 11:44:20 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Merge 2 sorted linked list in reverse order/merge-2-sorted-linked-list-in-reverse-order.cpp
@@ -0,0 +1,144 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+void print(struct Node *Node)
+{
+    while (Node!=NULL)
+    {
+        cout << Node->data << " ";
+        Node = Node->next;
+    }
+}
+
+struct Node * mergeResult(struct Node *node1,struct Node *node2);
+
+Node *newNode(int data)
+{
+    Node *temp = new Node;
+    temp->data = data;
+    temp->next = NULL;
+    return temp;
+}
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int nA;
+        cin>>nA;
+        int nB;
+        cin>>nB;
+
+        struct Node* headA=NULL;
+        struct Node* tempA = headA;
+
+        for(int i=0;i<nA;i++)
+        {
+            int ele;
+            cin>>ele;
+            if(headA==NULL)
+            {
+                headA=tempA=newNode(ele);
+
+            }else{
+                tempA->next = newNode(ele);
+				tempA=tempA->next;
+            }
+        }
+
+        struct Node* headB=NULL;
+        struct Node* tempB = headB;
+
+
+        for(int i=0;i<nB;i++)
+        {
+            int ele;
+            cin>>ele;
+            if(headB==NULL)
+            {
+                headB=tempB=newNode(ele);
+
+            }else{
+                tempB->next = newNode(ele);
+				tempB=tempB->next;
+            }
+        }
+
+        struct Node* result = mergeResult(headA,headB);
+
+        print(result);
+        cout<<endl;
+
+
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+int data;
+Node* next;
+};
+
+*/
+struct Node* rev(Node* head){
+    Node* curr=NULL;
+    Node * prev=NULL;
+    Node* next=head;
+    
+    while(next){
+        curr=next;
+        next=next->next;
+        curr->next=prev;
+        prev=curr;
+    }
+    return curr;
+}
+struct Node * mergeResult(Node *node1,Node *node2)
+{
+    // your code goes here
+    Node* p1=rev(node1);
+    Node* p2=rev(node2);
+    Node* dummy=newNode(-1);
+    Node* p3=dummy;
+    
+    while(p1 and p2){
+        if(p1->data>p2->data){
+            p3->next=p1;
+            p1=p1->next;
+            p3=p3->next;
+        }else{
+            p3->next=p2;
+            p2=p2->next;
+            p3=p3->next;
+        }
+    }
+    while(p1){
+        p3->next=p1;
+        p1=p1->next;
+        p3=p3->next;
+    }
+    while(p2){
+        p3->next=p2;
+        p2=p2->next;
+        p3=p3->next;
+    }
+    p3->next=NULL;
+    return dummy->next;
+}
