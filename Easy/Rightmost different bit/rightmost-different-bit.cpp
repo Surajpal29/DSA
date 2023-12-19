2dbafc0ab21ae8839ad0ee1804db3169be4b145f
suraj pal <102373413+Surajpal29@users.noreply.github.com>
2023-12-19 13:27:06 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Rightmost different bit/rightmost-different-bit.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+ int posOfRightMostDiffBit(int m, int n)
+
+    {   
+
+        if(m ==n) return -1;
+
+        
+
+        int Xor = m^n;
+
+  
+
+        for(int i=0; i<32; i++){
+
+            
+
+            int mask = 1<<i;
+
+            if((mask & Xor) !=0)
+
+               return i+1;
+
+  
+
+        }
+
+        return -1;
+
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{   
+    int t;
+    cin>>t; //input number of testcases
+    while(t--)
+    {
+         int m,n;
+         cin>>m>>n; //input m and n
+         Solution ob;
+         cout << ob.posOfRightMostDiffBit(m, n)<<endl;
+    }
+    return 0;     
+} 
+// } Driver Code Ends
