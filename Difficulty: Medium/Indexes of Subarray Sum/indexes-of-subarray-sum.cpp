df5a297d67c20ea85e8eb431f1a9a73931cb0322
suraj pal <102373413+Surajpal29@users.noreply.github.com>
2024-08-27 17:41:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.cpp
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.cpp
@@ -9,30 +9,17 @@ class Solution {
     // Function to find a continuous sub-array which adds up to a given number.
     vector<int> subarraySum(vector<int> arr, int n, long long s) {
         // Your code here
-        int j=0;
-        long long sum =0;
-        vector<int>ans;
-        for(int i=0;i<arr.size();i++){
-           sum = sum+arr[i]; // Add current element to sum
-         
-          // If sum exceeds s, remove elements from the start until sum <= s
-           while(sum>s && j<i){
-             sum = sum-arr[j];
-             j++;
-           }
-          // If sum equals s, store the indices and break
-         if(sum == s){
-             ans.push_back(j+1);
-             ans.push_back(i+1);
-             break;
-         }
-         
-     }
-      // If ans is empty, return {-1} indicating no subarray found
-     if(ans.size() == 0){
-         return {-1};
-     }
-          return ans;
+        int left=0,right=0;
+        long long sum=0;
+        for(int i=0,j=0;i<n;i++){
+            sum+=arr[i];
+            while(sum>s && j<i){
+                sum-=arr[j];
+                j++;
+            }
+            if(sum==s) return {j+1,i+1};
+        }
+        return {-1};
     }
 };
 
