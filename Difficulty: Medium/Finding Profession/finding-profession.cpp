3d77fbfcebcffdeb2a3df4a541fabe7085e8e6da
suraj pal <102373413+Surajpal29@users.noreply.github.com>
2026-08-30 16:27:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Finding Profession/finding-profession.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    string profession(int level, int pos) {
+        // code here
+        int flips=0;
+        while(pos>1){
+            if((pos&1)==0) flips++;
+            pos=(pos+1)/2;
+        }
+        return (flips&1)?"Doctor":"Engineer";
+    }
+};
